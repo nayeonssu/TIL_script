d007e07df5e39f37a7f5a72245a9ce6d97ba6dfe
nayeonssu <sys7552@gmail.com>
2025-05-27 16:08:57 +0900

    프로젝트 뷰 완성

--- js_level1_portfolio/scripts/portfolio.js
+++ js_level1_portfolio/scripts/portfolio.js
@@ -16,23 +16,30 @@ popupBg.style.display = 'none';//display:none;과 같다.
 //변수로 만든 DOM요소가 여러개일 경우 이벤트를 위해 접근할때는 인덱스를 사용해서 하나씩 개별 접근해야한다. 인덱스(번호)
 thumbNailA[0].addEventListener('click',function(){
     //익명함수로
-    console.log(0);//작동 테스트
+    console.log(this);//작동 테스트
     console.log(this);//이벤트객체 자동인식하는지 확인
     console.log(this.children[0].src);//현재(this) 객체의 자식(childern)의 src를 찍어라
     console.log(popupBg.children[0].children[0].src);//팝업bg의 자식의 자식의 src확인
-    popupShow(); //팝업 출력 함수 호출
+    popupShow(this); //팝업 출력 함수 호출
     //클릭하고 싶은 이미지 넣고 싶을때
-    popupBg.children[0].children[0].src = this.children[0].src//대입되는 위치
+    
 });
 thumbNailA[1].addEventListener('click',function(){
-    popupShow();
-    popupBg.children[0].children[0].src = this.children[0].src
+    popupShow(this);
 });
-thumbNailA[2].addEventListener('click',popupShow);
-thumbNailA[3].addEventListener('click',popupShow);
-thumbNailA[4].addEventListener('click',popupShow);
-function popupShow() {
-    return popupBg.style.display = 'flex';//flex와 block이 다르다. | flex로 디자인을 해서 다시 flex작성해주면 된다.(원래 가지고 있던 속성을 그대로 넣는것이다.)
+thumbNailA[2].addEventListener('click',function(){
+    popupShow(this);
+});
+thumbNailA[3].addEventListener('click',function(){
+    popupShow(this);
+});
+function popupShow(target) {
+    console.log('----------------------함수시작위치와 에러체크');
+    popupBg.style.display = 'flex';//flex와 block이 다르다. | flex로 디자인을 해서 다시 flex작성해주면 된다.(원래 가지고 있던 속성을 그대로 넣는것이다.)
+    //console.log(this.children[0]);//정상체크
+    popupBg.children[0].children[0].src = target.children[0].src//대입되는 위치
+    //함수를 작동할때마다 this를 새로 작성해야한다.
+    return //함수 종료 표시
 }
 //3. 배경 클릭 시 팝업 숨기기
 popupBg.addEventListener('click',popupHide);
