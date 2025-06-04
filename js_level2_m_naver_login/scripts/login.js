cfbe13ab169c2a748024bdb40f1324b69a824098
nayeonssu <sys7552@gmail.com>
2025-06-04 10:19:29 +0900

    로그인 에러 조건1완성

--- js_level2_m_naver_login/scripts/login.js
+++ js_level2_m_naver_login/scripts/login.js
@@ -21,3 +21,32 @@ langSelect.addEventListener('click',()=>{
 
 //.lang_open이 보이는 상태라면 #lang_select 클릭 시 .lang_open 숨기기
 //변수생성  -> 이벤트 제작 -> 이벤트 만족 시 실행함수(중간중간 콘솔 자주 체크)
+//네이버 로그인 에러 메세지 알고리즘
+/* 
+
+조건2) 아이디를 입력하고 비밀번호를 입력하지 않고 로그인 클릭 시
+조건2 결과) 비밀번호를 입력해 주세요.
+조건3) 아이디, 비밀번호를 모두 입력하고 로그인 클릭 시 정보가 틀렸을 때
+조건3 결과) 아이디(로그인 전화번호, 로그인 전용 아이디) 또는 비밀번호가 잘못 되었습니다. 아이디와 비밀번호를 정확히 입력해 주세요.
+조건4)  조건3 상황에서 로그인실패가 3회 이상일때
+조건4 결과) 영수증 퀴즈와 '아이디(로그인 전화번호, 로그인 전용 아이디) 또는 비밀번호가 잘못 되었습니다. 아이디와 비밀번호를 정확히 입력해 주세요.'
+다시 로그인해 주세요
+*/
+//조건1) 아이디와 비밀번호를 모두 입력하지 않고 로그인 클릭 시
+//조건1 결과) 아이디 또는 전화번호를 입력해 주세요.
+const userId = document.querySelector('input[name=user_id]')
+const userPw = document.querySelector('input[name=user_pw]')
+const errorMsg = document.querySelector('.error_msg')
+const loginBtn = document.querySelector('#login_btn')
+console.log(userId, userPw, errorMsg, loginBtn)
+loginBtn.addEventListener('click',()=>{
+    //유저 아이디도 빈 문자열이고, 비밀번호도 빈 문자열일 때 //&&
+    /* if(userId.value == '' && userPw.value == ''){
+        errorMsg.textContent = '아이디 또는 전화번호를 입력해 주세요.'
+        //input -> value | 다른 태그 -> textContent
+    } */
+    //삼항조건 활용 조건1
+    //조건식 ? 조건식참결과 : 조건식거짓결과
+    //(아래)조건결과대입변수 = 조건식 ? 조건식참결과 : 조건식거짓결과
+    errorMsg.textContent = userId.value == '' && userPw.value == '' ? '아이디 또는 전화번호를 입력해 주세요.':'';
+})
